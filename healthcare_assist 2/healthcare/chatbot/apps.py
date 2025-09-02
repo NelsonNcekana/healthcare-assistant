8d2bbac5ecc96dd4f1f5a0c03f3431165989cf26
Nelson Ncekana <nelson.ncekana@gmail.com>
2025-09-02 21:44:38 +0200

    Delete healthcare_assist 2/healthcare directory

--- healthcare_assist 2/healthcare/chatbot/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChatbotConfig(AppConfig):
-    """Configuration for the chatbot app."""
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'healthcare.chatbot'
